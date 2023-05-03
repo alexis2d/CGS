68a21922e237578ab5a526c16a7c0bb784337e98
Alexis Duquenet <gxgwyllaz37@gmail.com>
2023-05-03 11:20:34 +0200

    classroom crud

---
+++ src/main/java/fr/cgs/cgs_back/controller/ClassroomController.java
@@ -0,0 +1,57 @@
+package fr.cgs.cgs_back.controller;
+
+import com.electronwill.nightconfig.core.conversion.Path;
+import fr.cgs.cgs_back.dto.ClassroomDto;
+import fr.cgs.cgs_back.entity.Classroom;
+import fr.cgs.cgs_back.service.ClassroomService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/classroom")
+public class ClassroomController {
+
+    @Autowired
+    private ClassroomService classroomService;
+
+    @GetMapping("/{id}")
+    public Classroom findById(@PathVariable int id) {
+        return classroomService.findById(id);
+    }
+
+    @GetMapping("/list")
+    public List<Classroom> list() {
+        return classroomService.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Classroom> insertClassroom(@RequestBody Classroom classroom) {
+        Classroom newClassroom = classroomService.insertClassroom(classroom);
+        return ResponseEntity.ok(newClassroom);
+    }
+
+    @PutMapping("/{id}/update")
+    public ResponseEntity<Classroom> updateClassroom(@PathVariable int id, @RequestBody Classroom updatedClassroom) {
+        try{
+            Classroom classroom = classroomService.findById(id);
+            classroom.setName(updatedClassroom.getName());
+            classroom.setCapacity(updatedClassroom.getCapacity());
+            classroom.setSite(updatedClassroom.getSite());
+            Classroom savedClassroom = classroomService.insertClassroom(classroom);
+            return ResponseEntity.ok(savedClassroom);
+        } catch(EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void deleteClassroom(@PathVariable int id) {
+        classroomService.deleteClassroom(id);
+    }
+
+}
--- src/main/java/fr/cgs/cgs_back/dto/ClassroomDto.java
+++ src/main/java/fr/cgs/cgs_back/dto/ClassroomDto.java
@@ -1,14 +1,11 @@
 package fr.cgs.cgs_back.dto;
 
 public class ClassroomDto {
-
-    private int id;
     private String name;
     private int capacity;
     private SiteDto site;
 
-    public ClassroomDto(int id, String name, int capacity, SiteDto site) {
-        this.id = id;
+    public ClassroomDto(String name, int capacity, SiteDto site) {
         this.name = name;
         this.capacity = capacity;
         this.site = site;
