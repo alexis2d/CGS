d5957624c612082bc382872fe824bfc50e8e35b1
matthouff <berthelotmatthias@gmail.com>
2023-05-04 16:48:26 +0200

    ajout du crossOrigin

--- src/main/java/fr/cgs/cgs_back/controller/ReservationController.java
+++ src/main/java/fr/cgs/cgs_back/controller/ReservationController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/reservation")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReservationController {
 
     @Autowired
